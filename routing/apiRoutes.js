e49e8f53551f7cb8c8449c40d86b34ac5f523b9b
Mkelsey <mkh13b@my.fsu.edu>
2018-08-15 11:51:57 -0400

    Matching score math is correct

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,6 +1,6 @@
 var friends = require("../app/data/friends.js");
 var userDifferenceScores = [];
-
+var score = 0;
 module.exports = function(app){
     app.get('/api/friends', function(request, response) {
     response.json(friends);
@@ -12,12 +12,7 @@ module.exports = function(app){
             //friends.push(request.body.scores);
             response.json(true);
             console.log("It worked?");
-           /* for(var i = 0; i < request.body.scores.length; i++) {
-                if(request[0].body.scores[i] > request[i + 1].body.scores[i]) {
-                    var diff = request[0].body.scores[i] - request[i+1].body.scores[i]
-                    console.log(diff);
-                }
-            }*/
+
             
 
             for(var i = 0; i < friends.length; i++) {
@@ -26,89 +21,75 @@ module.exports = function(app){
                 var potentialMatchSpace = currentUserSpace - i;
                 console.log(friends[currentUserSpace]);
                 console.log(friends[potentialMatchSpace]); 
-                var score = 0;
+                
                 for(var e = 0; e < friends[currentUserSpace].scores.length; e++) {
-                    function difference (a, b) {
+                    function difference (a, b,) {
+                        //a = parseInt(friends[currentUserSpace].scores[e]);
+                        //b = parseInt(friends[potentialMatchSpace].scores[e]);
                         a = parseInt(friends[currentUserSpace].scores[e]);
-                        b = parseInt(friends[potentialMatchSpace].scores[e]); 
+                        b = parseInt(friends[potentialMatchSpace].scores[e]);
                        var newScore = Math.abs(a - b);
-                       score = score + newScore;
+                       console.log("NEW SCORE");
+                       console.log(newScore);
+                       score += parseInt(newScore);
+                       console.log("SCORE: " + score);
+                       
                        //console.log(score);
-                       if(i === friends.length - 1 && e === friends[currentUserSpace].scores.length - 1) {
+
+                        if(i === friends.length - 1 && e === friends[currentUserSpace].scores.length - 1) {
                         userDifferenceScores.push(score);
                         console.log(userDifferenceScores);
+                        score = 0;
+                        console.log(bestMatch(userDifferenceScores));
+                        var finalMatch = userDifferenceScores[1];
+                        console.log("Final Match: " + finalMatch);
+                        userDifferenceScores = [];
                     }
+                    else if(e === friends[currentUserSpace].scores.length - 1) {
+                        userDifferenceScores.push(score);
+                        score = 0;
+                       }
                     else {
-                        console.log("Still calculationg... please hold.");
+                        console.log("Still calculating... please hold.");
                     }
                    }
                    difference();
                    
 
-
-                    
-                    //console.log(friends[currentUserSpace].scores[e]);
-                    //console.log(friends[potentialMatchSpace].scores[e]);
-                    /*if(parseInt(friends[currentUserSpace].scores[e] > parseInt(friends[potentialMatchSpace].scores[e]))) {
-                        score = score + parseInt(friends[currentUserSpace].scores[e]) - parseInt(friends[potentialMatchSpace].scores[e]);
-                        if(i === friends.length - 1 && e === friends[currentUserSpace].scores.length - 1) {
-                            userDifferenceScores.push(score);
-                            console.log(userDifferenceScores);
-                        }
-                        else {
-                            console.log("Still calculating... please hold.");
-                        }
-                    }
-                    else {
-                        score = score + parseInt(friends[potentialMatchSpace].scores[e]) - parseInt(friends[currentUserSpace].scores[e]);
-                        if(i === friends.length - 1 && e === friends[currentUserSpace].scores.length - 1) {
-                            userDifferenceScores.push(score);
-                            console.log(userDifferenceScores);
-                        }
-                        else {
-                            console.log("Still calculating... please hold.");
-                        }
-                    }*/
-
-                   /* if( i === friends.length - 1 && e === friends[currentUserSpace].scores.length - 1 && parseInt(friends[currentUserSpace].scores[e]) > parseInt(friends[potentialMatchSpace].scores[e])) {
-                        score = score + parseInt(friends[currentUserSpace].scores[e]) - parseInt(friends[potentialMatchSpace].scores[e]);
-                        userDifferenceScores.push(score);
-                        console.log(userDifferenceScores);
-                    }
-                    else if( i === friends.length - 1 && e === friends[currentUserSpace].scores.length - 1 && parseInt(friends[potentialMatchSpace].scores[e]) > parseInt(friends[currentUserSpace].scores[e])) {
-                        score = score + parseInt(friends[potentialMatchSpace].scores[e]) - parseInt(friends[currentUserSpace].scores[e]);
-                        userDifferenceScores.push(score);
-                        console.log(userDifferenceScores);
-                    }
-                    else if(parseInt(friends[currentUserSpace].scores[e]) > parseInt(friends[potentialMatchSpace].scores[e])) {
-                        score = score + parseInt(friends[currentUserSpace].scores[e]) - parseInt(friends[potentialMatchSpace].scores[e]);
-
-                    }
-                    else {
-                        score = score + parseInt(friends[potentialMatchSpace].scores[e]) - parseInt(friends[currentUserSpace].scores[e]);
-                    }*/
-                     
-                    //console.log(score);
-
                 }
+                console.log(userDifferenceScores);
                 
                 
-                //console.log(friends[potentialMatchSpace].scores);
-                /*for(var e = 0; e < friends[potentialMatchSpace].scores.length; i++) {
-                    console.log(friends[currentUserSpace].scores[i]);
-                    console.log("BREAK");
-                    //console.log(friends[currentUserSpace].scores[i]);
-                    console.log(friends[currentUserSpace - i].scores[i]);
-                    //console.log(friends[potentialMatchSpace].scores[i]);
+ 
+                function bestMatch (items) {
+                    for (var i = 0; i < items.length; i++) {
+                      let value = items[i]
+                      // store the current item value so it can be placed right
+                      for (var j = i - 1; j > -1 && items[j] > value; j--) {
+                        // loop through the items in the sorted array (the items from the current to the beginning)
+                        // copy each item to the next one
+                        items[j + 1] = items[j]
+                      }
+                      // the last item we've reached should now hold the value of the currently sorted item
+                      items[j + 1] = value
+                    }
+                  
+                    return userDifferenceScores
+                  }
+                  
+                  
+                  
 
-                     
-                };*/
                    
             };
+
+            //displayMatch();
+
 
     });
     
 
 }
+
 
 
